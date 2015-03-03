a76754cf07f59372927cd5ed7ccd90db5693e5a0
Paul Cohn <pscohn@gmail.com>
2015-03-03 15:07:01 -0500

    No open register

--- routes/login.js
+++ routes/login.js
@@ -9,12 +9,6 @@ router.get('/', function(req, res, next) {
 router.post('/', function(req, res, next) {
     if (req.params.login != null) {
 
-    } else if (req.params.register != null) {
-        bcyrpt.genSalt(10, function(err, salt) {
-            bcrypt.hash(req.params.password, salt, function(err, hash) {
-                
-            });
-        });
     }    
 });
 
