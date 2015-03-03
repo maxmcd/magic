ef0a7f86507d3b7e557ee5caa189a5e81b54618c
Paul Cohn <pscohn@gmail.com>
2015-03-03 14:50:55 -0500

    Beginnings of pw hash

--- routes/login.js
+++ routes/login.js
@@ -3,12 +3,21 @@ var router = express.Router();
 var bcrypt = require('bcrypt');
 
 router.get('/login', function(req, res, next) {
-    res.render('index', { title: 'Express' });
+    res.render('login', { title: 'Express' });
 });
 
 router.post('/login', function(req, res, next) {
-        
+    if (req.params.login != null) {
+
+    } else if (req.params.register != null) {
+        bcyrpt.genSalt(10, function(err, salt) {
+            bcrypt.hash(req.params.password, salt, function(err, hash) {
+                
+            });
+        });
+    }    
 });
+
 
 
 module.exports = router;
