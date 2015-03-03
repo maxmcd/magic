36d407d514c78d1d208c830f4c9ca5f5ef8668ad
Paul Cohn <pscohn@gmail.com>
2015-03-03 15:07:52 -0500

    Initial magicians model

---
+++ models/magicians.js
@@ -0,0 +1,21 @@
+var Sequelize = require('sequelize')
+var database = require('../database')
+
+var Magician = database.define('magicians', {
+    email: {
+        type: Sequelize.STRING,
+        field: 'email''
+    },
+    password: {
+        type: Sequelize.STRING,
+        field: 'password'
+    }
+}, {
+    freezeTableName: true 
+});
+
+Magician.sync({force: true}).then(function () {
+  // Table created
+});
+
+module.exports = Magician;
