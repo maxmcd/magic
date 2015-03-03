ec348d4ec9164f0ac6bad021d44287e436691ecd
Paul Cohn <pscohn@gmail.com>
2015-03-03 18:00:07 -0500

    Check for successful login

--- routes/login.js
+++ routes/login.js
@@ -1,15 +1,23 @@
 var express = require('express');
 var router = express.Router();
 var bcrypt = require('bcrypt');
+var Magician = require('../models/magicians');
 
 router.get('/', function(req, res, next) {
     res.render('login', { title: 'Login' });
 });
 
 router.post('/', function(req, res, next) {
-    if (req.params.login != null) {
-
-    }    
+    Magician.find({ where: {email: req.body.email} }).then(function(magician) {
+        bcrypt.compare(req.body.password, magician.password, function(err, match) {
+            if (match == true) {
+                res.redirect('/dashboard');
+            } else {
+                console.log('fail auth');
+                res.redirect('/login');
+            }
+        });
+    });
 });
 
 module.exports = router;
