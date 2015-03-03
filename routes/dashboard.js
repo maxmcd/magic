7b62d55c1fd69c1f5deb9f240264458689095abf
maxmcd <max.t.mcdonnell@gmail.com>
2015-03-03 17:22:36 -0500

    rough dashboard styles and layout

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-    res.render('dashboard', { title: 'Login' });
+    res.render('dashboard', { title: 'dashboard' });
 });
 
 
