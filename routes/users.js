64d48e7a36911b8c2313ae90f09b56299ba1386d
maxmcd <max.t.mcdonnell@gmail.com>
2015-03-04 19:33:01 -0500

    accept user credit card on /users/:id/cc

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/users')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+    res.send('respond with a resource');
 });
+
+router.get('/:id/cc/', function(req, res, next) {
+    var id = req.param('id');
+    res.render('cc', {userId: id})
+})
+
+router.post('/:id/cc/', function(req, res, next) {
+    console.log(req)
+    var id = req.param('id');
+    var token = req.body["token[id]"]
+    User.find(id).then(function(user) {
+        user.stripeId = token
+        user.save().then(function(user) {
+            res.json(true)           
+        })
+    })
+})
 
 module.exports = router;
