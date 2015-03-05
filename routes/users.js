6f5ea984d2855d76aa44c49ba0e257f15276f8cf
maxmcd <max.t.mcdonnell@gmail.com>
2015-03-04 19:47:29 -0500

    charge card endpoint(untested)

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var User = require('../models/users')
+var stripe = require('stripe')(process.env.STRIPE_MAGIC_API_KEY);
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -22,6 +23,28 @@ router.post('/:id/cc/', function(req, res, next) {
             res.json(true)           
         })
     })
+})
+
+router.post("/:id/charge/", function(req, res, next) {
+    var amount = req.body.amount
+    
+    var formattedAmount = Math.round(amount*100)
+    // stripe needs 1040, not 10.40
+
+    User.find(id).then(function(user) {
+        stripe.charges.create({
+          amount: formattedAmount,
+          currency: "usd",
+          source: user.stripeId, // obtained with Stripe.js
+          description: "Charge for Magic"
+        }, function(err, charge) {
+            if (!err) {
+                res.json(true)
+                return
+            }
+        });
+    })
+    res.status(500).send('Something broke!');
 })
 
 module.exports = router;
